1afa454297b06f7f74648a6f61302f67336cb860
James Bielman <jbielman@common-lisp.net>
2003-09-29 00:25:18 +0000

    * swank-openmcl.lisp: New file, a Slime backend for OpenMCL 0.14.x.

---
+++ swank-openmcl.lisp
@@ -0,0 +1,520 @@
+;;;; -*- Mode: lisp; indent-tabs-mode: nil -*-
+;;;
+;;; openmcl-swank.lisp --- SLIME backend for OpenMCL.
+;;;
+;;; Copyright (C) 2003, James Bielman  <jamesjb@jamesjb.com>
+;;;
+;;; This program is licensed under the terms of the Lisp Lesser GNU
+;;; Public License, known as the LLGPL, and distributed with OpenMCL
+;;; as the file "LICENSE".  The LLGPL consists of a preamble and the
+;;; LGPL, which is distributed with OpenMCL as the file "LGPL".  Where
+;;; these conflict, the preamble takes precedence.
+;;;
+;;; The LLGPL is also available online at
+;;; http://opensource.franz.com/preamble.html
+;;;
+;;;   $Id$
+;;;
+
+;;;
+;;; This is the beginning of a Slime backend for OpenMCL.  It has been
+;;; tested only with OpenMCL version 0.14-030901 on Darwin --- I would
+;;; be interested in hearing the results with other versions.
+;;;
+;;; Things that work:
+;;;
+;;; * Evaluation of forms with C-M-x.
+;;; * Compilation of defuns with C-c C-c.
+;;; * File compilation with C-c C-k.
+;;; * Basic debugger functionality, jumping to frames is not implemented yet.
+;;; * Macroexpanding with C-c RET.
+;;; * Disassembling the symbol at point with C-c M-d.
+;;; * Describing symbol at point with C-c C-d.
+;;;
+;;; Things that sort of work:
+;;;
+;;; * Compiler warnings are trapped and sent to Emacs.  The elisp code
+;;;   attempts to place the warnings on the correct defun by doing
+;;;   regular expression searches.  Does not work in all cases.
+;;;
+;;; Things that aren't done yet:
+;;;
+;;; * Cross-referencing.
+;;; * Symbol completion.
+;;; * Apropos.
+;;; * Due to unimplementation functionality the test suite does not
+;;;   run correctly (it hangs upon entering the debugger).
+;;;
+
+;;; Administrivia
+
+(defpackage :swank
+  (:use :common-lisp)
+  (:export #:start-server))
+
+(in-package :swank)
+
+(defconstant server-port 4005
+  "Default port for the Swank TCP server.")
+
+(defvar *swank-debug-p* t
+  "When true, print extra debugging information.")
+
+;;; Setup and Hooks
+
+(defun start-server (&optional (port server-port))
+  "Start the Slime backend on TCP port `port'."
+  (create-swank-server port :reuse-address t))
+
+;;; TCP Server
+
+;; In OpenMCL, the Swank backend runs in a separate thread and simply
+;; blocks on its TCP port while waiting for forms to evaluate.
+
+(defvar *emacs-io* nil
+  "Bound to a TCP stream to Emacs during request processing.")
+
+(defvar *slime-output* nil
+  "Bound to a slime-output-stream during request processing.")
+
+(defun create-swank-server (port &key reuse-address)
+  "Create a Swank TCP server on `port'."
+  (ccl:process-run-function "Swank Request Processor" #'swank-main-loop
+                            port reuse-address))
+
+(defun swank-main-loop (port reuse-address)
+  "Create the TCP server and accept connections in a new thread."
+  (let ((server-socket (ccl:make-socket :connect :passive :local-port port
+                                        :reuse-address reuse-address)))
+    (format *terminal-io* "~&;; Swank: Accepting connections on port ~D.~%"
+            port)
+    (loop
+     (let ((socket (ccl:accept-connection server-socket :wait t)))
+       (ccl:process-run-function
+        (list :name (format nil "Swank Client ~D" (ccl:socket-os-fd socket))
+              :initial-bindings `((*emacs-io* . ',socket)))
+        #'request-loop)))))
+
+(defun request-loop ()
+  "Thread function for a single Swank connection.  Processes requests
+until the remote Emacs goes away."
+  (unwind-protect
+       (loop
+        (catch 'slime-toplevel
+          (with-simple-restart (abort "Return to Slime event loop.")
+            (let ((completed nil))
+              (let ((*slime-output* (make-instance 'slime-output-stream)))
+                (let ((condition (catch 'serve-request-catcher
+                                   (read-from-emacs)
+                                   (setq completed t))))
+                  (unless completed
+                    (when *swank-debug-p*
+                      (format *debug-io*
+                              "~&;; Connection to Emacs lost.~%;; [~A]~%"
+                              condition))
+                    (return))))))))
+    (format *terminal-io* "~&;; Swank: Closed connection: ~A~%" *emacs-io*)
+    (close *emacs-io*)))
+
+(defun read-from-emacs ()
+  "Read and process a request from Emacs."
+  (let ((form (read-next-form)))
+    (let ((*standard-output* *slime-output*)
+          (*error-output* *slime-output*)
+          (*trace-output* *slime-output*)
+          (*debug-io* *slime-output*)
+          (*query-io* *slime-output*))
+      (apply #'funcall form))))
+
+(defun read-next-form ()
+  "Read the next Slime request from *EMACS-IO* and return an
+S-expression to be evaulated to handle the request.  If an error
+occurs during parsing, it will be noted and control will be tranferred
+back to the main request handling loop."
+  (handler-case
+      (let* ((length (logior (ash (read-byte *emacs-io*) 16)
+                             (ash (read-byte *emacs-io*) 8)
+                             (read-byte *emacs-io*)))
+             (string (make-string length)))
+        (read-sequence string *emacs-io*)
+        (read-form string))
+    (condition (c)
+      (throw 'serve-request-catcher c))))
+
+(defvar *swank-io-package*
+  (let ((package (make-package "SWANK-IO-PACKAGE")))
+    (import 'nil package)
+    package))
+
+(defun read-form (string)
+  (with-standard-io-syntax
+    (let ((*package* *swank-io-package*))
+      (read-from-string string))))
+
+(defun send-to-emacs (object)
+  "Send `object' to Emacs."
+  (let* ((string (prin1-to-string-for-emacs object))
+         (length (1+ (length string))))
+    (loop for position from 16 downto 0 by 8
+          do (write-byte (ldb (byte 8 position) length) *emacs-io*))
+    (write-string string *emacs-io*)
+    (terpri *emacs-io*)
+    (force-output *emacs-io*)))
+
+(defun prin1-to-string-for-emacs (object)
+  (let ((*print-case* :downcase)
+        (*print-readably* nil)
+        (*print-pretty* nil)
+        (*package* *swank-io-package*))
+    (prin1-to-string object)))
+
+;;; Redirecting Output to Emacs
+
+;; This buffering is done via a Gray stream instead of the CMU-specific
+;; stream method business...
+(defclass slime-output-stream (ccl::fundamental-character-output-stream)
+  ((buffer :initform (make-string-output-stream :element-type 'character)
+           :accessor slime-output-stream-buffer)))
+
+(defmethod ccl:stream-write-char ((stream slime-output-stream) char)
+  (write-char char (slime-output-stream-buffer stream)))
+
+(defmethod ccl:stream-line-column ((stream slime-output-stream))
+  0)
+
+(defmethod ccl:stream-force-output ((stream slime-output-stream))
+  (send-to-emacs `(:read-output ,(get-output-stream-string
+                                  (slime-output-stream-buffer stream))))
+  (setf (slime-output-stream-buffer stream) (make-string-output-stream)))
+
+;;; Utilities
+
+(defvar *buffer-package*)
+
+(defun from-string (string)
+  "Read string in the *BUFFER-PACKAGE*"
+  (let ((*package* *buffer-package*))
+    (read-from-string string)))
+
+(defun to-string (string)
+  "Write string in the *BUFFER-PACKAGE*."
+  (let ((*package* *buffer-package*))
+    (prin1-to-string string)))
+
+(defmacro defslimefun (fun &rest rest)
+  `(progn
+    (defun ,fun ,@rest)
+    (export ',fun :swank)))
+
+(defmacro defslimefun-unimplemented (fun args)
+  `(progn
+    (defun ,fun ,args
+      (declare (ignore ,@args))
+      (error "Backend function ~A not implemented." ',fun))
+    (export ',fun :swank)))
+
+;;; Evaluation
+
+(defvar *swank-debugger-condition*)
+(defvar *swank-debugger-hook*)
+(defvar *swank-debugger-stack-frame*)
+
+;;; XXX i hope this is correct for threads
+(defmethod ccl::application-error :before (application condition error-pointer)
+  (declare (ignore application condition))
+  (setq *swank-debugger-stack-frame* error-pointer))
+
+(defun swank-debugger-hook (condition hook)
+  (let ((*swank-debugger-condition* condition)
+        (*swank-debugger-hook* hook))
+    (sldb-loop)))
+
+(defun guess-package-from-string (name)
+  (or (and name
+           (or (find-package name)
+               (find-package (string-upcase name))))
+      *package*))
+
+(defslimefun eval-string (string buffer-package)
+  (let ((*debugger-hook* #'swank-debugger-hook))
+    (let (ok result)
+      (unwind-protect
+           (let ((*buffer-package* (guess-package-from-string buffer-package)))
+             (assert (packagep *buffer-package*))
+             (setq result (eval (read-form string)))
+             (force-output)
+             (setq ok t))
+        (send-to-emacs (if ok `(:ok ,result) '(:aborted)))))))
+
+(defslimefun interactive-eval (string)
+  (let ((values (multiple-value-list (eval (from-string string)))))
+    (force-output)
+    (format nil "~{~S~^, ~}" values)))
+
+(defslimefun-unimplemented interactive-eval-region (string))
+(defslimefun-unimplemented pprint-eval (string))
+(defslimefun-unimplemented re-evaluate-defvar (form))
+
+(defslimefun arglist-string (fname)
+  (let ((*print-case* :downcase))
+    (multiple-value-bind (function condition)
+        (ignore-errors (values (from-string fname)))
+      (when condition
+        (return-from arglist-string (format nil "(-- ~A)" condition)))
+      (let ((arglist (ccl:arglist function)))
+        (if arglist
+            (princ-to-string arglist)
+            "(-- <Unknown-Function>)")))))
+
+;;; Compilation
+
+(defvar *compiler-notes* '())
+
+(defun clear-compiler-notes ()
+  (setf *compiler-notes* '()))
+
+(defun condition-function-name (condition)
+  "Return the function name as a symbol from a compiler condition."
+  (symbol-name (car (ccl::compiler-warning-function-name
+                     condition))))
+
+(defun handle-compiler-warning (condition)
+  "Construct a compiler note for Emacs from a compiler warning
+condition."
+  (describe (car (ccl::compiler-warning-function-name condition)))
+  (push (list :position nil
+              :function-name (condition-function-name condition)
+              :source-path nil
+              :filename (ccl::compiler-warning-file-name condition)
+              :severity :warning
+              :message (format nil "~A" condition)
+              :context nil
+              :buffername nil
+              :buffer-offset 0)
+        *compiler-notes*))
+
+(defun unix-gettimeofday ()
+  (ccl::rlet ((tv :timeval))
+    (#_gettimeofday tv (ccl::%null-ptr))
+    (values (ccl::pref tv :timeval.tv_sec)
+            (ccl::pref tv :timeval.tv_usec))))
+
+(defun measure-time-interval (fn)
+  "Call FN and return the first return value aand the elapsed time.
+The time is measured in microseconds."
+  (multiple-value-bind (start-secs start-usecs)
+      (unix-gettimeofday)
+    (let ((value (funcall fn)))
+      (multiple-value-bind (end-secs end-usecs)
+          (unix-gettimeofday)
+        (values value (+ (* (- end-secs start-secs) 1000000)
+                         (- end-usecs start-usecs)))))))
+
+(defmacro with-trapping-compilation-notes (() &body body)
+  `(handler-bind ((ccl::compiler-warning #'handle-compiler-warning))
+    ,@body))
+
+(defun call-with-compilation-hooks (fn)
+  (multiple-value-bind (result usecs)
+      (with-trapping-compilation-notes ()
+        (clear-compiler-notes)
+        (measure-time-interval fn))
+    (list (to-string result)
+          (format nil "~,2F" (/ usecs 1000000.0)))))
+
+(defslimefun swank-compile-string (string buffer start)
+  (declare (ignore buffer start))
+  (call-with-compilation-hooks
+   (lambda ()
+     (let ((*package* *buffer-package*))
+       (eval (from-string
+              (format nil "(funcall (compile nil '(lambda () ~A)))"
+                      string)))))))
+
+(defslimefun swank-compile-file (filename load)
+  (call-with-compilation-hooks
+   (lambda ()
+     (compile-file filename :load load))))
+
+(defslimefun compiler-notes-for-emacs ()
+  (reverse *compiler-notes*))
+
+(defslimefun-unimplemented compiler-notes-for-file (filename))
+
+;;; Debugging
+
+(defvar *sldb-level* 0)
+(defvar *sldb-stack-top*)
+(defvar *sldb-restarts*)
+
+(defslimefun ping (level)
+  (cond ((= level *sldb-level*)
+         *sldb-level*)
+        (t
+         (throw-to-toplevel))))
+
+(defslimefun getpid ()
+  (ccl::getpid))
+
+(defslimefun sldb-loop ()
+  (let* ((*sldb-level* (1+ *sldb-level*))
+         (*sldb-stack-top* nil)
+         ;; This is a complete hack --- since we're not running at top level we
+         ;; don't want to publish the last restart to Emacs which would allow
+         ;; the user to break outside of the request loop.  What's the right
+         ;; way to do this?
+         (*sldb-restarts* (butlast
+                           (compute-restarts *swank-debugger-condition*)))
+         (*debugger-hook* nil)
+         (level *sldb-level*)
+         (*package* *buffer-package*))
+    (send-to-emacs (list* :debug *sldb-level* (debugger-info-for-emacs 0 1)))
+    (unwind-protect
+         (loop
+          (catch 'sldb-loop-catcher
+            (with-simple-restart (abort "Return to sldb level ~D." level)
+              (read-from-emacs))))
+      (send-to-emacs `(:debug-return ,level)))))
+
+(defun format-restarts-for-emacs ()
+  (loop for restart in *sldb-restarts*
+        collect (list (princ-to-string (restart-name restart))
+                      (princ-to-string restart))))
+
+(defun format-condition-for-emacs ()
+  (format nil "~A~%   [Condition of type ~S]"
+          *swank-debugger-condition* (type-of *swank-debugger-condition*)))
+
+;; This is deep voodoo copied from ccl:lib/backtrace.lisp --- ideally
+;; OpenMCL would provide a function for copying backtrace info into a
+;; vector or something.
+(defun frame-parameters (p tcr lfun pc)
+  (with-output-to-string (s)
+    (multiple-value-bind (count vsp parent-vsp)
+        (ccl::count-values-in-frame p tcr)
+      (declare (fixnum count))
+      (dotimes (i count)
+        (multiple-value-bind (var type name)
+            (ccl::nth-value-in-frame p i tcr lfun pc vsp parent-vsp)
+          (declare (ignore name type))
+          (format s " ~S" var))))))
+
+;; Also copied almost verbatim from the OpenMCL sources.
+(defun compute-backtrace (start end &key (start-frame (ccl::%get-frame-ptr)))
+  (let ((tcr (ccl::%current-tcr))
+        (result)
+        (total 0))
+    (do* ((p start-frame (ccl::parent-frame p tcr))
+          (frame-number 0 (1+ frame-number))
+          (q (ccl::last-frame-ptr tcr)))
+         ((or (null p) (eq p q) (ccl::%stack< q p tcr))
+          (values))
+      (declare (fixnum frame-number))
+      (progn
+        (multiple-value-bind (lfun pc) (ccl::cfp-lfun p)
+          (incf total)
+          (if (and (>= frame-number start) (< frame-number end))
+              (push (list frame-number
+                          (format nil "~D: (~A~A)"
+                                  frame-number
+                                  (if lfun
+                                      (ccl::%lfun-name-string lfun)
+                                      "#<Unknown Frame>")
+                                  (if lfun
+                                      (frame-parameters p tcr lfun pc)
+                                      "")))
+                    result)))))
+    (values (nreverse result) total)))
+
+(defslimefun backtrace-for-emacs (start end)
+  (compute-backtrace start end :start-frame *swank-debugger-stack-frame*))
+
+(defslimefun debugger-info-for-emacs (start end)
+  (multiple-value-bind (backtrace length)
+      (backtrace-for-emacs start end)
+    (list (format-condition-for-emacs)
+          (format-restarts-for-emacs)
+          length backtrace)))
+
+(defun nth-restart (index)
+  (nth index *sldb-restarts*))
+
+(defslimefun invoke-nth-restart (index)
+  (let ((restart (nth-restart index)))
+    (invoke-restart restart)))
+
+(defslimefun sldb-continue ()
+  (continue *swank-debugger-condition*))
+
+(defslimefun sldb-abort ()
+  (invoke-restart (find 'abort *sldb-restarts* :key #'restart-name)))
+
+(defslimefun throw-to-toplevel ()
+  (throw 'slime-toplevel nil))
+
+;;; Utilities
+
+(defun print-output-to-string (fn)
+  (with-output-to-string (*standard-output*)
+    (let ((*debug-io* *standard-output*))
+      (funcall fn))))
+
+(defun print-description-to-string (object)
+  (print-output-to-string (lambda () (describe object))))
+
+(defslimefun describe-symbol (symbol-name)
+  (print-description-to-string (from-string symbol-name)))
+
+(defslimefun describe-function (symbol-name)
+  (print-description-to-string (from-string symbol-name)))
+
+(defslimefun-unimplemented describe-setf-function (symbol-name))
+(defslimefun-unimplemented describe-type (symbol-name))
+
+(defslimefun describe-class (symbol-name)
+  (print-description-to-string (find-class (from-string symbol-name) nil)))
+
+(defslimefun features ()
+  (mapcar #'symbol-name *features*))
+
+(defslimefun-unimplemented apropos-list-for-emacs (name &optional
+                                                        external-only
+                                                        package))
+
+;;; Tracing and Disassembly
+
+(defslimefun disassemble-symbol (symbol-name)
+  (print-output-to-string
+   (lambda () (disassemble (from-string symbol-name)))))
+
+;;; Cross-referencing
+
+;; I think some of these will never work in OpenMCL...
+(defslimefun-unimplemented who-calls (symbol-name package-name))
+(defslimefun-unimplemented who-references (symbol-name package-name))
+(defslimefun-unimplemented who-binds (symbol-name package-name))
+(defslimefun-unimplemented who-sets (symbol-name package-name))
+(defslimefun-unimplemented who-macroexpands (symbol-name package-name))
+
+(defslimefun-unimplemented find-fdefinition (symbol-name package-name))
+(defslimefun-unimplemented function-source-location-for-emacs (fname))
+
+;;; Completion
+
+(defslimefun-unimplemented completions (string default-package-name))
+(defslimefun-unimplemented list-all-package-names ())
+
+;;; Macroexpansion
+(defun apply-macro-expander (expander string)
+  (let ((*print-pretty* t)
+        (*print-length* 20)
+        (*print-level* 20))
+    (to-string (funcall expander (from-string string)))))
+
+(defslimefun swank-macroexpand-1 (string)
+  (apply-macro-expander #'macroexpand-1 string))
+
+(defslimefun swank-macroexpand (string)
+  (apply-macro-expander #'macroexpand string))
+
+(defslimefun-unimplemented swank-macroexpand-all (string))
