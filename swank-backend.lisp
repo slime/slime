6ec17769cb4046a9a9496703e2c87ba38568e47f
Helmut Eller <heller@common-lisp.net>
2004-01-09 19:42:25 +0000

    (debugger-info-for-emacs, find-function-locations): Doc fix.

--- swank-backend.lisp
+++ swank-backend.lisp
@@ -219,13 +219,13 @@ user without (re)entering the debugger by wrapping them as
   (:documentation
    "Return debugger state, with stack frames from START to END.
 The result is a list:
-  (condition-description ({restart}*) ({stack-frame}*)
+  (condition ({restart}*) ({stack-frame}*)
 where
+  condition   ::= (description type)
   restart     ::= (name description)
   stack-frame ::= (number description)
 
-condition-description---a string describing the condition that
-triggered the debugger.
+condition---a pair of strings: message, and type.
 
 restart---a pair of strings: restart name, and description.
 
@@ -236,12 +236,12 @@ Below is an example return value. In this case the condition was a
 division by zero (multi-line description), and only one frame is being
 fetched (start=0, end=1).
 
- (\"Arithmetic error DIVISION-BY-ZERO signalled.
-Operation was KERNEL::DIVISION, operands (1 0).
-   [Condition of type DIVISION-BY-ZERO]\"
+ ((\"Arithmetic error DIVISION-BY-ZERO signalled.
+Operation was KERNEL::DIVISION, operands (1 0).\"
+   \"[Condition of type DIVISION-BY-ZERO]\")
   ((\"ABORT\" \"Return to Slime toplevel.\")
    (\"ABORT\" \"Return to Top-Level.\"))
-  ((0 \"0: (KERNEL::INTEGER-/-INTEGER 1 0)\")))"))
+  ((0 \"(KERNEL::INTEGER-/-INTEGER 1 0)\")))"))
 
 (defgeneric backtrace (start end)
   (:documentation
@@ -337,7 +337,10 @@ LOCATION is a source location of the form:
              | (:source-form <string>)
 
 <position> ::= (:position <fixnum> [<align>]) ; 1 based
-             | (:function-name <string>)"))
+             | (:line <fixnum> [<fixnum>])
+             | (:function-name <string>)
+             | (:source-path <list> <start-position>)
+"))
 
 
 ;;;; Multiprocessing
