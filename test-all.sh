5add08a8d201db0ef824a71e202d51d874778488
Helmut Eller <heller@common-lisp.net>
2004-03-09 20:12:43 +0000

    New file.

---
+++ test-all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+trap EXIT
+
+for emacs in xemacs ; do # emacs-20.7 emacs-21.3.50 xemacs ; do
+  for lisp in 'cmucl -noinit' sbcl lispworks-personal-4300 'clisp -K full' acl5; do
+  echo testing: $emacs $lisp dribble.$emacs_$lisp result.$emacs_$lisp 
+  test.sh $emacs "$lisp" "dribble.${emacs}_${lisp}" "result.${emacs}_${lisp}"
+ done
+done
+    
