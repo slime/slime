0b91f1f1cda3079e45b7d9fba933129ba3de0d49
Luke Gorrie <lgorrie@common-lisp.net>
2004-06-29 20:18:05 +0000

    New shell script for creating a tarball for distribution.

---
+++ mkdist.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+version="1.0alpha0"
+dist="slime-$version"
+
+if [ -d $dist ]; then rm -rf $dist; fi
+
+mkdir $dist
+cp README HACKING ChangeLog *.el *.lisp $dist/
+
+mkdir $dist/doc
+cp doc/Makefile doc/slime.texi doc/texinfo-tabulate.awk $dist/doc
+
+tar czf $dist.tar.gz $dist
