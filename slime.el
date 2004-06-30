3de34f87b39e6c45567a141b5d39abbb3bc21b12
Marco Baringer <mbaringer@common-lisp.net>
2004-06-30 07:24:20 +0000

    2004-06-29  Thomas Burdick  <tfb@OCF.Berkeley.EDU>
    	* slime.el: Indicate when the REPL is in the debugger's context
    	(slime-debug-level): new connection var
    	(slime-dispatch-event): set slime-debug-level to match *sldb-level*
    	(slime-repl-insert-prompt): show debug-level in prompt when > 0

--- slime.el
+++ slime.el
@@ -1256,6 +1256,9 @@ Polling %S.. (Abort with `M-x slime-abort-connection'.)"
     (string-match "^[^ ]*" name)
     (capitalize (match-string 0 name))))
 
+(slime-def-connection-var slime-debug-level 0
+  "The current level of recursive debugging.")
+
 
 (defvar slime-words-of-encouragement
   `("Let the hacking commence!"
@@ -1602,9 +1605,11 @@ fixnum a specific thread."))
        (sldb-activate thread level))
       ((:debug thread level condition restarts frames)
        (assert thread)
+       (setf (slime-debug-level) level)
        (sldb-setup thread level condition restarts frames))
       ((:debug-return thread level)
        (assert thread)
+       (setf (slime-debug-level) (1- level))
        (sldb-exit thread level))
       ((:emacs-interrupt thread)
        (cond ((slime-use-sigint-for-interrupt) (slime-send-sigint))
@@ -2168,7 +2173,9 @@ end end."
     (slime-insert-propertized '(face slime-repl-result-face) result)
     (unless (bolp) (insert "\n"))
     (let ((prompt-start (point))
-          (prompt (format "%s> "  (slime-lisp-package))))
+          (prompt (if (> (slime-debug-level) 0)
+                      (format "%s [%d]> " (slime-lisp-package) (slime-debug-level))
+                      (format "%s> "  (slime-lisp-package)))))
       (slime-propertize-region
           '(face slime-repl-prompt-face
                  read-only t
