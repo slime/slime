b6302ce32dbfbb05c9ce930884dde46d4f7653b8
Helmut Eller <heller@common-lisp.net>
2004-09-03 21:20:00 +0000

    Add PROBLEMS file.

--- mkdist.sh
+++ mkdist.sh
@@ -5,7 +5,7 @@ dist="slime-$version"
 if [ -d $dist ]; then rm -rf $dist; fi
 
 mkdir $dist
-cp NEWS README HACKING ChangeLog *.el *.lisp $dist/
+cp NEWS README HACKING PROBLEMS ChangeLog *.el *.lisp $dist/
 
 mkdir $dist/doc
 cp doc/Makefile doc/slime.texi doc/texinfo-tabulate.awk $dist/doc
