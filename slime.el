903cbab15208893c634ab5cde19c014d2f60dfbd
Luke Gorrie <lgorrie@common-lisp.net>
2004-11-24 03:51:32 +0000

    (slime-repl-mode-map): Add slime-load-file on `C-c C-l'.

--- slime.el
+++ slime.el
@@ -2898,6 +2898,7 @@ DIRECTION is 'forward' or 'backward' (in the history list)."
   ("\C-c\C-p" 'slime-repl-previous-prompt)
   ("\M-\C-a" 'slime-repl-beginning-of-defun)
   ("\M-\C-e" 'slime-repl-end-of-defun)
+  ("\C-c\C-l" 'slime-load-file)
   )
 
 (define-key slime-repl-mode-map
