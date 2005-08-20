f820a572f8f3e8ca44cd1d8f48796766d51c01ab
Christophe Rhodes <crhodes@common-lisp.net>
2005-08-20 19:36:13 +0000

    (noted by Brian Mastenbrook) non-Linux non-linkage-table sbcls try to get the address of linux_no_threads_p at fasl-load time, and complain vigorously if it's not available.  Guard with #+linux.

--- swank-sbcl.lisp
+++ swank-sbcl.lisp
@@ -43,6 +43,7 @@
 
 (defimplementation preferred-communication-style ()
   (if (and (member :sb-thread *features*)
+           #+linux
            (not (sb-alien:extern-alien "linux_no_threads_p" sb-alien:boolean)))
       :spawn
       :fd-handler))
