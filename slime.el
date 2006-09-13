d1e8b6a1ca4941b9e86ca18c84d41bacbba579ae
Marco Baringer <mbaringer@common-lisp.net>
2006-09-13 15:26:05 +0000

    *** empty log message ***

--- slime.el
+++ slime.el
@@ -193,7 +193,7 @@ the machine 'soren' and you can connect with the username
 
   (push (list \"^soren$\"
               (lambda (emacs-filename)
-                (subseq (length \"/ssh:animaliter@soren:\") emacs-filename))
+                (subseq emacs-filename (length \"/ssh:animaliter@soren:\")))
               (lambda (lisp-filename)
                 (concat \"/ssh:animaliter@soren:\" lisp-filename)))
         slime-filename-translations)
