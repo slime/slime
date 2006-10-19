0fdbdb4dae0427ba9d6325dfd7c28bcff861ab20
Helmut Eller <heller@common-lisp.net>
2006-10-19 12:29:32 +0000

    (ignored-xref-function-names): Deleted.

--- swank-backend.lisp
+++ swank-backend.lisp
@@ -732,12 +732,6 @@ The return value is as for WHO-CALLS.")
   "List the functions called by FUNCTION-NAME.
 See LIST-CALLERS for a description of the return value.")
 
-;;; Utilities
-
-(definterface ignored-xref-function-names ()
-  "List of function names that SANITIZE-XREFS should remove."
-  '(nil))
-
 
 ;;;; Profiling
 
