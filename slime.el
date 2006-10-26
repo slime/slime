0c6ebd81f5bbcbfb78220f1f52ea115a9405ddac
Marco Baringer <mbaringer@common-lisp.net>
2006-10-26 08:52:47 +0000

    (slime-global-variable-name-p): Use a custom 'parser' instead of string-match to avoid regexp overflow errors on very long strings.

--- slime.el
+++ slime.el
@@ -5632,7 +5632,20 @@ messages."
 (defun slime-global-variable-name-p (name)
   "Is NAME a global variable?
 Globals are recognised purely by *this-naming-convention*."
-  (string-match "^\\(.*::?\\)?[*+].*[*+]$" name))
+  (when (<= (length name) 2)
+    (return-from slime-global-variable-name-p nil))
+  (when (char-equal ?\: (aref name 0))
+    (return-from slime-global-variable-name-p nil))
+  (let ((package-prefix-end (cond
+                          ((search "::" name)
+                           (+ 2 (search "::" name)))
+                          ((search ":" name)
+                           (+ 1 (search ":" name)))
+                          (t
+                           0))))
+    (let ((first-char-in-name (aref name package-prefix-end)))
+      (and (member first-char-in-name '(?\* ?\+))
+           (char-equal first-char-in-name (aref name (1- (length name))))))))
 
 (defun slime-get-cached-autodoc (symbol-name)
   "Return the cached autodoc documentation for SYMBOL-NAME, or nil."
