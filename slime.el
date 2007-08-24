62ae7e151d1cbaefdac74e5dc92e5426ad48de9b
Helmut Eller <heller@common-lisp.net>
2007-08-24 13:43:02 +0000

    Move slime-highlight-edits-mode to contrib.

--- slime.el
+++ slime.el
@@ -67,9 +67,6 @@
 (defvar slime-use-autodoc-mode nil
   "When non-nil always enable slime-autodoc-mode in slime-mode.")
 
-(defvar slime-use-highlight-edits-mode nil
-  "When non-nil always enable slime-highlight-edits-mode in slime-mode")
-
 (defvar slime-highlight-compiler-notes t
   "When non-nil highlight buffers with compilation notes, warnings and errors."
   )
@@ -88,9 +85,7 @@
 (defun slime-shared-lisp-mode-hook ()
   (slime-mode 1)
   (when slime-use-autodoc-mode
-    (slime-autodoc-mode 1))
-  (when slime-use-highlight-edits-mode
-    (slime-highlight-edits-mode 1)))
+    (slime-autodoc-mode 1)))
 
 (defun slime-lisp-mode-hook ()
   (slime-shared-lisp-mode-hook)
@@ -5968,85 +5963,6 @@ annoy the user)."
   (interactive)
   (unless (slime-typeout-active-p)
     (slime-make-typeout-frame)))
-
-
-;;;; edit highlighting
-
-(defface slime-highlight-edits-face
-    `((((class color) (background light))
-       (:background "lightgray"))
-      (((class color) (background dark))
-       (:background "dimgray"))
-      (t (:background "yellow")))
-  "Face for displaying edit but not compiled code."
-  :group 'slime-mode-faces)
-
-(define-minor-mode slime-highlight-edits-mode 
-  "Minor mode to highlight not-yet-compiled code." nil)
-
-(add-hook 'slime-highlight-edits-mode-on-hook
-          'slime-highlight-edits-init-buffer)
-
-(add-hook 'slime-highlight-edits-mode-off-hook
-          'slime-highlight-edits-reset-buffer)
-
-(defun slime-highlight-edits-init-buffer ()
-  (make-local-variable 'after-change-functions)
-  (add-to-list 'after-change-functions 
-               'slime-highlight-edits)
-  (add-to-list 'slime-before-compile-functions
-               'slime-highlight-edits-compile-hook))
-
-(defun slime-highlight-edits-reset-buffer ()
-  (setq after-change-functions  
-        (remove 'slime-highlight-edits after-change-functions))
-  (slime-remove-edits (point-min) (point-max)))
-
-(defun slime-highlight-edits (beg end &optional len)
-  (save-match-data
-    (when (and (slime-connected-p)
-               (not (slime-inside-comment-p beg end))
-               (not (slime-only-whitespace-p beg end)))
-      (let ((overlay (make-overlay beg end)))
-        (overlay-put overlay 'face 'slime-highlight-edits-face)
-        (overlay-put overlay 'slime-edit t)))))
-
-(defun slime-remove-edits (start end)
-  "Delete the existing Slime edit hilights in the current buffer."
-  (save-excursion
-    (goto-char start)
-    (while (< (point) end)
-      (dolist (o (overlays-at (point)))
-        (when (overlay-get o 'slime-edit)
-          (delete-overlay o)))
-      (goto-char (next-overlay-change (point))))))
-
-(defun slime-highlight-edits-compile-hook (start end)
-  (when slime-highlight-edits-mode
-    (let ((start (save-excursion (goto-char start) 
-                                 (skip-chars-backward " \t\n\r")
-                                 (point)))
-          (end (save-excursion (goto-char end) 
-                               (skip-chars-forward " \t\n\r")
-                               (point))))
-      (slime-remove-edits start end))))
-
-(defun slime-inside-comment-p (beg end)
-  "Is the region from BEG to END in a comment?"
-  (save-excursion
-    (goto-char beg)
-    (let* ((hs-c-start-regexp ";\\|#|")
-           (comment (hs-inside-comment-p)))
-      (and comment
-           (destructuring-bind (cbeg cend) comment
-             (<= end cend))))))
-
-(defun slime-only-whitespace-p (beg end)
-  "Contains the region from BEG to END only whitespace?"
-  (save-excursion
-    (goto-char beg)
-    (skip-chars-forward " \n\t\r" end)
-    (<= end (point))))
 
 
 ;;;; Completion
