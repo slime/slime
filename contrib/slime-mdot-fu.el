7d44359d565b38c501d847243b170204b00f7f29
Tobias C. Rittweiler <trittweiler@common-lisp.net>
2008-08-07 14:49:51 +0000

    * slime-mdot-fu.el: Works for LET bindings now also.   (def-slime-test find-local-definitions.1): New test case.

--- contrib/slime-mdot-fu.el
+++ contrib/slime-mdot-fu.el
@@ -10,7 +10,8 @@
 (defvar slime-binding-ops-alist
   '((flet &bindings &body) 
     (labels &bindings &body)
-    (macrolet &bindings &body)))
+    (macrolet &bindings &body)
+    (let &bindings &body)))
 
 (defun slime-lookup-binding-op (op)
   (assoc* op slime-binding-ops-alist :test 'equalp :key 'symbol-name))
@@ -74,6 +75,48 @@ in a local function binding near point."
 (defun slime-mdot-fu-unload ()
   (remove-hook 'slime-edit-definition-hooks 
 	       'slime-edit-local-definition))
+
+
+
+(def-slime-test find-local-definitions.1
+    (buffer-sexpr definition target-regexp)
+    "Check that finding local definitions work."
+    '(((defun foo (x)
+	  (let ((y (+ x 1)))
+	    (- x y *HERE*)))
+       y
+       "(y (+ x 1))")
+
+      ((defun bar (x)
+	 (flet ((foo (z) (+ x z)))
+	   (* x (foo *HERE*))))
+       foo
+       "(foo (z) (+ x z))")
+
+      ((defun quux (x)
+	 (flet ((foo (z) (+ x z)))
+	   (let ((foo (- 1 x)))
+	     (+ x foo *HERE*))))
+       foo
+       "(foo (- 1 x)")
+      
+      ((defun zurp (x)
+	 (macrolet ((frob (x y) `(quux ,x ,y)))
+	   (frob x *HERE*)))
+       frob
+       "(frob (x y)"))
+  (slime-check-top-level)
+  (with-temp-buffer
+    (let ((tmpbuf (current-buffer)))
+      (insert (prin1-to-string buffer-sexpr))
+      (search-backward "*HERE*")
+      (slime-edit-local-definition (prin1-to-string definition))
+      (slime-sync)
+      (slime-check "Check that we didnt leave the temp buffer." 
+	(eq (current-buffer) tmpbuf))
+      (slime-check "Check that we are at the local definition."
+	(looking-at (regexp-quote target-regexp))))))
+
 
 (provide 'slime-mdot-fu)
 
