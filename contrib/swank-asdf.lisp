cbf60e6a43dce61d957cb60713a5efa921dce106
Tobias C. Rittweiler <trittweiler@common-lisp.net>
2008-09-10 23:51:17 +0000

    * swank-asdf.lisp (operate-on-system-for-emacs): Adapted to recent 	changes wrt. swank-compilation-unit.

--- contrib/swank-asdf.lisp
+++ contrib/swank-asdf.lisp
@@ -15,10 +15,10 @@
 (defslimefun operate-on-system-for-emacs (system-name operation &rest keywords)
   "Compile and load SYSTEM using ASDF.
 Record compiler notes signalled as `compiler-condition's."
-  (with-swank-compilation-unit (:override nil)
-    (swank-compiler 
-     (lambda ()
-       (apply #'operate-on-system system-name operation keywords)))))
+  (swank-compilation-result-for-emacs
+   (swank-compiler (make-swank-compilation-result)
+		   (lambda ()
+		     (apply #'operate-on-system system-name operation keywords)))))
 
 (defun operate-on-system (system-name operation-name &rest keyword-args)
   "Perform OPERATION-NAME on SYSTEM-NAME using ASDF.
