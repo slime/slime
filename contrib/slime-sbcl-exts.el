b7abaddcac4c1885103529470d13c365914601d2
Tobias C. Rittweiler <trittweiler@common-lisp.net>
2009-10-30 23:06:26 +0000

    `M-x slime-visit-sbcl-bug' will open a browser to visit SBCL's bug 	tracker at Launchpad that describes the bug number at 	point (#nnnnnn).
    	* slime-sbcl-exts.el (slime-visit-sbcl-bug): New.
    	(slime-read-sbcl-bug): New.
    	(slime-sbcl-bug-at-point): New.

--- contrib/slime-sbcl-exts.el
+++ contrib/slime-sbcl-exts.el
@@ -8,6 +8,29 @@
 (require 'slime-autodoc)
 (require 'slime-references)
 
+(defun slime-sbcl-bug-at-point ()
+  (save-excursion
+    (save-match-data
+      (unless (looking-at "#[0-9]\\{6\\}")
+        (search-backward-regexp "#\\<" (line-beginning-position) t))
+      (when (looking-at "#[0-9]\\{6\\}")
+        (buffer-substring-no-properties (match-beginning 0) (match-end 0))))))
+
+(defun slime-read-sbcl-bug (prompt &optional query)
+  "Either read a sbcl bug or choose the one at point.
+The user is prompted if a prefix argument is in effect, if there is no
+symbol at point, or if QUERY is non-nil."
+  (let ((bug (slime-sbcl-bug-at-point)))
+    (cond ((or current-prefix-arg query (not bug))
+           (slime-read-from-minibuffer prompt bug))
+          (t bug))))
+
+(defun slime-visit-sbcl-bug (bug)
+  "Visit the Launchpad site that describes `bug' (#nnnnnn)."
+  (interactive (list (slime-read-sbcl-bug "Bug number (#nnnnnn): ")))
+  (browse-url (format "http://bugs.launchpad.net/sbcl/+bug/%s" 
+                      (substring bug 1))))
+
 (defun slime-enable-autodoc-for-sb-assem:inst ()
   (push '("INST" . (slime-make-extended-operator-parser/look-ahead 1))
         slime-extended-operator-name-parser-alist))
