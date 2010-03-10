13f675179037da4d1e49e159ecc3051dcc6de4a5
Stas Boukarev <sboukarev@common-lisp.net>
2010-03-10 00:02:53 +0000

    * swank-ccl.lisp (emacs-inspect function): Print closed over variables in case of closure.

--- swank-ccl.lisp
+++ swank-ccl.lisp
@@ -628,6 +628,26 @@
                                  "Underlying UVECTOR"))))
               (t value)))))
 
+(defmethod emacs-inspect ((f function))
+  (append
+   (label-value-line "Name" (function-name f))
+   `("Its argument list is: " 
+     ,(princ-to-string (arglist f)) (:newline))
+   (label-value-line "Documentation" (documentation  f t))
+   (when (function-lambda-expression f)
+     (label-value-line "Lambda Expression"
+                       (function-lambda-expression f)))
+   (when (ccl:function-source-note f)
+     (label-value-line "Source note"
+                       (ccl:function-source-note f)))
+   (when (typep f 'ccl:compiled-lexical-closure)
+     (append
+      (label-value-line "Inner function" (ccl::closure-function f))
+      '("Closed over values:" (:newline))
+      (loop for (name value) in (ccl::closure-closed-over-values f)
+            append (label-value-line (format nil " ~a" name)
+                                     value))))))
+
 (defclass uvector-inspector ()
   ((object :initarg :object)))
 
