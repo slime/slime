0a888874fe35216abca0df7b0ad4187f10e61f68
Helmut Eller <heller@common-lisp.net>
2010-06-22 10:02:49 +0000

    * swank-loader.lisp (*architecture-features*): ECL uses :x86_64.

--- swank-loader.lisp
+++ swank-loader.lisp
@@ -55,7 +55,7 @@
     :unix))
 
 (defparameter *architecture-features*
-  '(:powerpc :ppc :x86 :x86-64 :amd64 :i686 :i586 :i486 :pc386 :iapx386
+  '(:powerpc :ppc :x86 :x86-64 :x86_64 :amd64 :i686 :i586 :i486 :pc386 :iapx386
     :sparc64 :sparc :hppa64 :hppa
     :pentium3 :pentium4
     :java-1.4 :java-1.5 :java-1.6 :java-1.7))
