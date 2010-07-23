7b53e4b81af05d6d5df16e94e8a984f482676efc
Stas Boukarev <sboukarev@common-lisp.net>
2010-07-23 01:41:27 +0000

    * slime-fancy-inspector.el (slime-fancy-inspector): Add slime-parse dependency.

--- contrib/slime-fancy-inspector.el
+++ contrib/slime-fancy-inspector.el
@@ -3,8 +3,8 @@
   "Fancy inspector for CLOS objects."
   (:authors "Marco Baringer <mb@bese.it> and others")
   (:license "GPL")
+  (:slime-dependencies slime-parse)
   (:swank-dependencies swank-fancy-inspector))
-
 
 (defun slime-inspect-definition ()
   "Inspect definition at point"
