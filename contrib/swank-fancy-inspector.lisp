8af4b079f80fa0c70fc55b7c680aa1f24fb57e9d
Stas Boukarev <sboukarev@common-lisp.net>
2012-04-06 18:08:30 +0000

    * swank-fancy-inspector.lisp (format-iso8601-time): Add 1 to the time-zone if DST value of decode-universal-time is T.

--- contrib/swank-fancy-inspector.lisp
+++ contrib/swank-fancy-inspector.lisp
@@ -765,24 +765,26 @@ SPECIAL-OPERATOR groups."
   `("Value: " ,(princ-to-string n)))
 
 (defun format-iso8601-time (time-value &optional include-timezone-p)
-    "Formats a universal time TIME-VALUE in ISO 8601 format, with
+  "Formats a universal time TIME-VALUE in ISO 8601 format, with
     the time zone included if INCLUDE-TIMEZONE-P is non-NIL"    
-    ;; Taken from http://www.pvv.ntnu.no/~nsaa/ISO8601.html
-    ;; Thanks, Nikolai Sandved and Thomas Russ!
-    (flet ((format-iso8601-timezone (zone)
-             (if (zerop zone)
-                 "Z"
-                 (multiple-value-bind (h m) (truncate (abs zone) 1.0)
-                   ;; Tricky.  Sign of time zone is reversed in ISO 8601
-                   ;; relative to Common Lisp convention!
-                   (format nil "~:[+~;-~]~2,'0D:~2,'0D"
-                           (> zone 0) h (round (* 60 m)))))))
+  ;; Taken from http://www.pvv.ntnu.no/~nsaa/ISO8601.html
+  ;; Thanks, Nikolai Sandved and Thomas Russ!
+  (flet ((format-iso8601-timezone (zone)
+           (if (zerop zone)
+               "Z"
+               (multiple-value-bind (h m) (truncate (abs zone) 1.0)
+                 ;; Tricky.  Sign of time zone is reversed in ISO 8601
+                 ;; relative to Common Lisp convention!
+                 (format nil "~:[+~;-~]~2,'0D:~2,'0D"
+                         (> zone 0) h (round (* 60 m)))))))
     (multiple-value-bind (second minute hour day month year dow dst zone)
-      (decode-universal-time time-value)
-      (declare (ignore dow dst))
+        (decode-universal-time time-value)
+      (declare (ignore dow))
       (format nil "~4,'0D-~2,'0D-~2,'0DT~2,'0D:~2,'0D:~2,'0D~:[~*~;~A~]"
               year month day hour minute second
-              include-timezone-p (format-iso8601-timezone zone)))))
+              include-timezone-p (format-iso8601-timezone (if dst
+                                                              (+ zone 1)
+                                                              zone))))))
 
 (defmethod emacs-inspect ((i integer))
           (append
