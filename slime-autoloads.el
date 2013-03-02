dc686957ec00f9ebab024ead58d27155676369db
Stas Boukarev <sboukarev@common-lisp.net>
2013-03-02 01:17:09 +0000

    * slime-autoloads.el: Don't load autoloads when slime is already loaded, there's no sense in doing so.

--- slime-autoloads.el
+++ slime-autoloads.el
@@ -12,33 +12,34 @@
 
 ;;; Code:
 
-(autoload 'slime "slime"
-  "Start a Lisp subprocess and connect to its Swank server." t) 
+(unless (featurep 'slime)
+  (autoload 'slime "slime"
+            "Start a Lisp subprocess and connect to its Swank server." t) 
 
-(autoload 'slime-mode "slime"
-  "SLIME: The Superior Lisp Interaction (Minor) Mode for Emacs." t)
+  (autoload 'slime-mode "slime"
+            "SLIME: The Superior Lisp Interaction (Minor) Mode for Emacs." t)
 
-(autoload 'slime-connect "slime"
-  "Connect to a running Swank server." t)
+  (autoload 'slime-connect "slime"
+            "Connect to a running Swank server." t)
 
-(autoload 'hyperspec-lookup "hyperspec" nil t)
+  (autoload 'hyperspec-lookup "hyperspec" nil t)
 
-(autoload 'slime-lisp-mode-hook "slime")
-(autoload 'slime-scheme-mode-hook "slime")
+  (autoload 'slime-lisp-mode-hook "slime")
+  (autoload 'slime-scheme-mode-hook "slime")
 
-(defvar slime-lisp-modes '(lisp-mode))
-(defvar slime-setup-contribs nil
-  "List of contribst to load.
+  (defvar slime-lisp-modes '(lisp-mode))
+  (defvar slime-setup-contribs nil
+    "List of contribst to load.
 Modified my slime-setup.")
 
-(defun slime-setup (&optional contribs)
-  "Setup Emacs so that lisp-mode buffers always use SLIME.
+  (defun slime-setup (&optional contribs)
+    "Setup Emacs so that lisp-mode buffers always use SLIME.
 CONTRIBS is a list of contrib packages to load."
-  (when (member 'lisp-mode slime-lisp-modes)
-    (add-hook 'lisp-mode-hook 'slime-lisp-mode-hook))
-  (setq slime-setup-contribs contribs)
-  (add-hook 'slime-load-hook 'slime-setup-contribs))
+    (when (member 'lisp-mode slime-lisp-modes)
+      (add-hook 'lisp-mode-hook 'slime-lisp-mode-hook))
+    (setq slime-setup-contribs contribs)
+    (add-hook 'slime-load-hook 'slime-setup-contribs))
 
-(provide 'slime-autoloads)
+  (provide 'slime-autoloads))
 
 ;;; slime-autoloads.el ends here
