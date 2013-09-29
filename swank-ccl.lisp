b090605e87e550fca40de0a3dc99bd50aad2f524
Helmut Eller <heller@common-lisp.net>
2013-09-29 13:45:42 +0000

    For CCL, also search definitions of p2 translators.
    * swank-ccl.lisp (p2-definitions): New.
    (find-definitions): Use it.

--- swank-ccl.lisp
+++ swank-ccl.lisp
@@ -549,13 +549,22 @@
   (let ((alpha (gethash name ccl::*nx1-alphatizers*)))
     (and alpha (ccl:find-definition-sources alpha))))
 
+(defun p2-definitions (name)
+  (let ((nx1-op (gethash name ccl::*nx1-operators*)))
+    (and nx1-op
+         (let ((p2 (aref (ccl::backend-p2-dispatch ccl::*target-backend*)
+                         nx1-op)))
+           (and p2
+                (ccl:find-definition-sources p2))))))
+
 (defimplementation find-definitions (name)
   (let ((defs (append (or (ccl:find-definition-sources name)
                           (and (symbolp name)
                                (fboundp name)
                                (ccl:find-definition-sources
                                 (symbol-function name))))
-                      (alphatizer-definitions name))))
+                      (alphatizer-definitions name)
+                      (p2-definitions name))))
     (loop for ((type . name) . sources) in defs
           collect (list (definition-name type name)
                         (source-note-to-source-location
