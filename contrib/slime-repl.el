72e724a0bfcceaf666ad4c407e8d531309e093be
Stas Boukarev <stassats@gmail.com>
2014-04-02 12:21:16 +0400

    * contrib/slime-repl.el (slime-repl-add-to-input-history): Fix whitespace clean up. Reported by Robert Brown.

--- contrib/slime-repl.el
+++ contrib/slime-repl.el
@@ -957,13 +957,14 @@ used with a prefix argument (C-u), doesn't switch back afterwards."
  (defvar slime-repl-input-history '()
    "History list of strings read from the REPL buffer."))
 
+(defun slime-trim-whitespace (string)
+  (replace-regexp-in-string "^[\t\n\s]*\\(.*?\\)[\t\n\s]*$" "\\1" string))
+
 (defun slime-repl-add-to-input-history (string)
   "Add STRING to the input history.
 Empty strings and duplicates are ignored."
   (when slime-repl-history-trim-whitespaces
-    (setq string (replace-regexp-in-string "[\t\n\s]*\\(.*?\\)[\t\n\s]*"
-                                           "\\1"
-                                           string)))
+    (setq string (slime-trim-whitespace string)))
   (unless (equal string "")
     (when slime-repl-history-remove-duplicates
       (setq slime-repl-input-history
