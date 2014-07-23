1fc6e121160222be52ca54504247afc2b719cdd1
João Távora <joaotavora@gmail.com>
2014-07-23 09:50:27 +0100

    Fix reference to old sly-repl-tests.el, introduce basic sly-mrepl-tests.el
    * .travis.yml (env): check-repl -> check-mrepl
    
    * contrib/test/sly-mrepl-tests.el: New file

---
+++ contrib/test/sly-mrepl-tests.el
@@ -0,0 +1,27 @@
+(require 'sly-mrepl)
+(require 'sly-tests)
+(require 'cl-lib)
+
+
+(defun sly-mrepl-tests--assert-prompt ()
+  (let ((proper-prompt-p nil))
+    (cl-loop 
+     repeat 5
+     when (string-match "CL-USER> *$" (buffer-string))
+     do (setq proper-prompt-p t)
+     (cl-return)
+     do (sleep-for 0 300))
+    (or proper-prompt-p
+        (ert-fail (format "Proper prompt not seen in time (saw last 20 chars as \"%s\")"
+                          (buffer-substring-no-properties (max (point-min)
+                                                               (- (point-max)
+                                                                  20))
+                                                          (point-max)))))))
+
+(define-sly-ert-test basic-repl-setup ()
+  (with-current-buffer (sly-mrepl-new)
+    (sly-mrepl-tests--assert-prompt)
+    (kill-buffer (current-buffer))))
+
+
+(provide 'sly-mrepl-tests)
