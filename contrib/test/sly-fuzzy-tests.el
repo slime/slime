fec94921c05a8fe79acd07c2cf5c6757407d6d7a
João Távora <joaotavora@gmail.com>
2014-09-08 09:15:33 +0100

    Be a little more lenient on ccl's sly-fuzzy

--- contrib/test/sly-fuzzy-tests.el
+++ contrib/test/sly-fuzzy-tests.el
@@ -19,7 +19,7 @@
       ("common-lisp" ("common-lisp-user:" "common-lisp:") "common-lisp"))
   (let ((completions (sly-fuzzy-completions prefix)))
     (loop with head = (cl-subseq (car completions) 0
-                                 (* 2.0 (length required-completions)))
+                                 (* 2.5 (length required-completions)))
           for required in required-completions
           unless (cl-find required head :key #'car :test #'string=)
           do (ert-fail (format "Expected to find \"%s\" in the group of %d first suggestions for completing \"%s\""
