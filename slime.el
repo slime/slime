afba5a3e67801e3b1ae0524940ad985f4e8f4882
Luís Oliveira <loliveira@common-lisp.net>
2014-10-06 23:00:37 +0100

    Release 2.10

--- slime.el
+++ slime.el
@@ -3,7 +3,7 @@
 ;; URL: https://github.com/slime/slime
 ;; Package-Requires: ((cl-lib "0.5"))
 ;; Keywords: languages, lisp, slime
-;; Version: 2.9
+;; Version: 2.10
 
 ;;;; License
 ;;     Copyright (C) 2003  Eric Marsden, Luke Gorrie, Helmut Eller
