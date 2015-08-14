bbca70df1ee03adb3590b5794aca9a1526ff422f
Stas Boukarev <stassats@gmail.com>
2015-08-14 17:06:41 +0300

    * swank/source-path-parser.lisp (make-source-recorder): Fix handling of recursive calls to macros producing the same forms. e.g. #+(and) (a)

--- swank/source-path-parser.lisp
+++ swank/source-path-parser.lisp
@@ -64,8 +64,17 @@ before and after of calling FN in the hashtable SOURCE-MAP."
       #+(or)
       (format t "[~D \"~{~A~^, ~}\" ~D ~D ~S]~%"
 	      start values end (char-code char) char)
-      (unless (null values)
-	(push (cons start end) (gethash (car values) source-map)))
+      (when values
+        (destructuring-bind (&optional existing-start &rest existing-end)
+            (car (gethash (car values) source-map))
+          ;; Some macros may return what a sub-call to another macro
+          ;; produced, e.g. "#+(and) (a)" may end up saving (a) twice,
+          ;; once from #\# and once from #\(. If the saved form
+          ;; is a subform, don't save it again.
+          (unless (and existing-start existing-end
+                       (<= start existing-start end)
+                       (<= start existing-end end))
+            (push (cons start end) (gethash (car values) source-map)))))
       (values-list values))))
 
 (defun make-source-recording-readtable (readtable source-map)
