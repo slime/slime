a689890d3ff2a922db02c972e68b7960038f17ac
Stas Boukarev <stassats@gmail.com>
2015-11-11 00:42:23 +0300

    * swank.lisp (parse-package): Use parse-string instead of read-from-string.

--- swank.lisp
+++ swank.lisp
@@ -1637,8 +1637,7 @@ Return the symbol and a flag indicating whether the symbols was found."
 Return the package or nil."
   ;; STRING comes usually from a (in-package STRING) form.
   (ignore-errors
-    (find-package (let ((*package* *swank-io-package*))
-                    (read-from-string string)))))
+    (find-package (parse-string string *swank-io-package*))))
 
 (defun unparse-name (string)
   "Print the name STRING according to the current printer settings."
