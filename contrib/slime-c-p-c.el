slime/slime

commit 3467ebc9ead3408c21cd1d2ae119ac05e2a7e2d9

Solve remaining byte-compilation problems
* slime-tests.el (require): Add necessary require statements to
avoid warnings. Don't use xemacs compatibility layer.

* slime.el (slime-require): Move up to avoid byte-compiler warning.
(slime-setup, slime--setup-contribs): Use `slime-contribs'
directly.
(sldb-in-face): Renamed from `in-sldb-face'. Rename in users.

* lib/hyperspec.el: Require `cl-lib' instead of `cl'.

The contrib's Makefile now works as the top-level Makefile,
byte-compiling files separately, uncovering errors when trying to
byte-compile files in isolation. Fixed almost all such problems in
contrib's el files and remove uses of the "xemacs compability layer",
in preparation of its removal.

For most files this amounted to moving some definitions around,
requiring the needed libs at the top, and prefixing cl-lib functions
with "cl". `cl' is required at compile-time only for macros which
aren't found in cl-lib.

In slime.el, slime-tests.el and lib/hyperspec.el a few tweaks also
removed the last few warnings.

* contrib/Makefile (%.elc): Do as in root-makefile and compile each el
file separately. This reveals more warnings.

* contrib/bridge.el (bridge-insert-function): new variable to solve
redefinition hack in contrib/slime-presentations.el
(bridge-insert): use it.

* contrib/slime-asdf.el
(slime-open-system): Don't use `called-interactive-p'.

* contrib/slime-autodoc.el (slime-autodoc-maybe-enable): move to solve
compilation warnings.
(slime-autodoc-mode): Replace `interactive-p' call with new
`interactive' arg.

* contrib/slime-cl-indent.el (require): Require needed libraries,
including `slime'.
(common-lisp-active-style): use proper defvar.
(common-lisp-current-package-function): Make it a variable, which
is how it's used.
(common-lisp-run-indentation-tests): Use `insert-file-contents'.
(slime-cl-indent): provide `slime-cl-indent' as well as `cl-indent'

* contrib/slime-clipboard.el (slime-clipboard-display-entries): Don't use

contrib/slime-c-p-c.el
@@ -1,4 +1,5 @@
 (require 'slime)
+(require 'cl-lib)
 
 (defvar slime-c-p-c-init-undo-stack nil)
 
@@ -82,7 +83,7 @@ If false, move point to the end of the inserted text."
 	     (when slime-c-p-c-unambiguous-prefix-p
 	       (let ((unambiguous-completion-length
 		      (loop for c in completion-set
-			    minimizing (or (mismatch completed-prefix c)
+			    minimizing (or (cl-mismatch completed-prefix c)
 					   (length completed-prefix)))))
 		 (goto-char (+ beg unambiguous-completion-length))))
              (slime-display-or-scroll-completions completion-set 
@@ -128,7 +129,7 @@ current buffer."
         ;; completion.
         ))
      ((and (>= (length token) 2)
-           (string= (subseq token 0 2) "#\\"))
+           (string= (cl-subseq token 0 2) "#\\"))
       ;; Character name completion
       (return-from slime-contextual-completions
         (slime-completions-for-character token))))
@@ -142,9 +143,9 @@ current buffer."
   (slime-eval `(swank:completions-for-keyword ,prefix ',buffer-form)))
 
 (defun slime-completions-for-character (prefix)
-  (flet ((append-char-syntax (string) (concat "#\\" string)))
+  (cl-labels ((append-char-syntax (string) (concat "#\\" string)))
     (let ((result (slime-eval `(swank:completions-for-character
-                                ,(subseq prefix 2)))))
+                                ,(cl-subseq prefix 2)))))
       (when (car result)
         (list (mapcar 'append-char-syntax (car result))
               (append-char-syntax (cadr result)))))))

contrib/Makefile
@@ -9,7 +9,13 @@ LOAD_PATH=-L . -L ..
 CONTRIBS = $(patsubst slime-%.el,%,$(wildcard slime-*.el))
 SLIME_VERSION=$(shell grep "Version:" ../slime.el | grep -E -o "[0-9.]+$$")
 
-compile:
+ELFILES := $(shell find . -type f -iname "*.el")
+ELCFILES := $(patsubst %.el,%.elc,$(ELFILES))
+
+%.elc: %.el
+	$(EMACS) -Q $(LOAD_PATH) --batch -f batch-byte-compile $<
+
+compile: $(ELCFILES)
 	$(EMACS) -Q --batch $(LOAD_PATH) \
 	        --eval "(batch-byte-recompile-directory 0)" .
 

contrib/bridge.el
@@ -128,10 +128,15 @@ function can use this variable also.")
 (eval'able); last item is error condition which resulted.  This is provided
 to help handler-writers in their debugging.")
 
+(defvar bridge-insert-function nil
+  "If non-nil use this instead of `bridge-insert'")
+
 ;;;%Utilities
-(defun bridge-insert (output)
+(defun bridge-insert (output &optional _dummy)
   "Insert process OUTPUT into the current buffer."
-  (if output
+  (if bridge-insert-function
+      (funcall bridge-insert-function output)
+    (if output
       (let* ((buffer (current-buffer))
 	     (process (get-buffer-process buffer))
 	     (mark (process-mark process))
@@ -152,7 +157,7 @@ to help handler-writers in their debugging.")
 		    (save-excursion
 		      (select-window window)
 		      (recenter '(center))
-		      (select-window original)))))))))
+		      (select-window original))))))))))
 
 ;;;
 ;(defun bridge-send-string (process string)

contrib/inferior-slime.el
@@ -11,6 +11,7 @@
 ;;   (add-hook 'slime-load-hook (lambda () (require 'inferior-slime)))
 ;;   (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode 1)))
 (require 'slime)
+(require 'cl-lib)
 
 (define-minor-mode inferior-slime-mode
   "\\<slime-mode-map>\

contrib/slime-asdf.el
@@ -1,5 +1,6 @@
-(eval-and-compile
-  (require 'slime))
+(require 'slime)
+(require 'cl-lib)
+(require 'grep)
 
 (define-slime-contrib slime-asdf
   "ASDF support."
@@ -63,7 +64,7 @@ in the directory of the current buffer."
          (directory-files (file-name-directory directory) nil "\.asd$")))
     (loop for system in asd-files
           for candidate = (file-name-sans-extension system)
-          when (find candidate system-names :test #'string-equal)
+          when (cl-find candidate system-names :test #'string-equal)
             do (return candidate))))
 
 (defun slime-determine-asdf-system (filename buffer-package)
@@ -114,11 +115,11 @@ buffer's working directory"
   (interactive (list (slime-read-system-name)))
   (slime-oos system 'load-op))
 
-(defun slime-open-system (name &optional load)
+(defun slime-open-system (name &optional load interactive)
   "Open all files in an ASDF system."
-  (interactive (list (slime-read-system-name)))
+  (interactive (list (slime-read-system-name) nil t))
   (when (or load
-            (and (called-interactively-p)
+            (and interactive
                  (not (slime-eval `(swank:asdf-system-loaded-p ,name)))
                  (y-or-n-p "Load it? ")))
     (slime-load-system name))

contrib/slime-autodoc.el
@@ -1,4 +1,8 @@
 (require 'slime)
+(require 'eldoc)
+(require 'cl-lib)
+(eval-when-compile
+  (require 'cl))
 
 (define-slime-contrib slime-autodoc
   "Show fancy arglist in echo area."
@@ -18,17 +22,10 @@
    (dolist (h '(slime-mode-hook slime-repl-mode-hook sldb-mode-hook))
      (remove-hook h 'slime-autodoc-maybe-enable))))
 
-(defun slime-autodoc-maybe-enable ()
-  (when slime-use-autodoc-mode
-    (slime-autodoc-mode 1)
-    (setq slime-echo-arglist-function
-          (lambda ()
-            (if slime-autodoc-mode
-                (eldoc-message (slime-autodoc))
-                (slime-show-arglist))))))
-
 (defcustom slime-use-autodoc-mode t
-  "When non-nil always enable slime-autodoc-mode in slime-mode.")
+  "When non-nil always enable slime-autodoc-mode in slime-mode."
+  :type 'boolean
+  :group 'slime-ui)
 
 (defcustom slime-autodoc-use-multiline-p nil
   "If non-nil, allow long autodoc messages to resize echo area display."
@@ -42,7 +39,9 @@
 
 (defcustom slime-autodoc-accuracy-depth 10
   "Number of paren levels that autodoc takes into account for
-  context-sensitive arglist display (local functions. etc)")
+  context-sensitive arglist display (local functions. etc)"
+  :type 'integer
+  :group 'slime-ui)
 
 
 
@@ -125,8 +124,8 @@ Return DOCUMENTATION."
 
 ;;;; slime-autodoc-mode
 
-(defun* slime-autodoc (&optional (multilinep slime-autodoc-use-multiline-p)
-                                 cache-multiline)
+(cl-defun slime-autodoc (&optional (multilinep slime-autodoc-use-multiline-p)
+                                   cache-multiline)
   "Returns the cached arglist information as string, or nil.
 If it's not in the cache, the cache will be updated asynchronously."
   (interactive)
@@ -137,8 +136,8 @@ If it's not in the cache, the cache will be updated asynchronously."
     (save-match-data
       (unless (if (fboundp 'slime-repl-inside-string-or-comment-p)
                   (slime-repl-inside-string-or-comment-p)
-                  (slime-inside-string-or-comment-p))
-        (multiple-value-bind (cache-key retrieve-form)
+                (slime-inside-string-or-comment-p))
+        (cl-multiple-value-bind (cache-key retrieve-form)
             (slime-make-autodoc-rpc-form)
           (let* (cached
                  (multilinep (or (slime-autodoc-multiline-cached
@@ -146,26 +145,27 @@ If it's not in the cache, the cache will be updated asynchronously."
                                  multilinep)))
             (slime-autodoc-cache-multiline (car cache-key) cache-multiline)
             (cond
-              ((not cache-key) nil)
-              ((setq cached (slime-get-cached-autodoc cache-key))
-               (slime-format-autodoc cached multilinep))
-              (t
-               ;; If nothing is in the cache, we first decline (by
-               ;; returning nil), and fetch the arglist information
-               ;; asynchronously.
-               (slime-eval-async retrieve-form
-                 (lexical-let ((cache-key cache-key)
-                               (multilinep multilinep))
-                   (lambda (doc)
-                     (destructuring-bind (doc cache-p) doc
-                       (unless (eq doc :not-available)
-                         (when cache-p
-                           (slime-store-into-autodoc-cache cache-key doc))
-                         ;; Now that we've got our information,
-                         ;; get it to the user ASAP.
-                         (eldoc-message
-                          (slime-format-autodoc doc multilinep)))))))
-               nil))))))))
+             ((not cache-key) nil)
+             ((setq cached (slime-get-cached-autodoc cache-key))
+              (slime-format-autodoc cached multilinep))
+             (t
+              ;; If nothing is in the cache, we first decline (by
+              ;; returning nil), and fetch the arglist information
+              ;; asynchronously.
+              (slime-eval-async retrieve-form
+                (lexical-let
+                 ((cache-key cache-key)
+                  (multilinep multilinep))
+                 (lambda (doc)
+                   (cl-destructuring-bind (doc cache-p) doc
+                     (unless (eq doc :not-available)
+                       (when cache-p
+                         (slime-store-into-autodoc-cache cache-key doc))
+                       ;; Now that we've got our information,
+                       ;; get it to the user ASAP.
+                       (eldoc-message
+                        (slime-format-autodoc doc multilinep)))))))
+              nil))))))))
 
 (defvar slime-autodoc-cache-car nil)
 
@@ -181,6 +181,22 @@ If it's not in the cache, the cache will be updated asynchronously."
                      slime-autodoc-cache-car))
          (setq slime-autodoc-cache-car nil))))
 
+(make-variable-buffer-local (defvar slime-autodoc-mode nil))
+
+;; FIXME: use `define-minor-mode'.
+(defun slime-autodoc-mode (&optional arg interactive)
+  (interactive (list (or current-prefix-arg 'toggle) t))
+  (make-local-variable 'eldoc-documentation-function)
+  (make-local-variable 'eldoc-idle-delay)
+  (make-local-variable 'eldoc-minor-mode-string)
+  (setq eldoc-documentation-function 'slime-autodoc)
+  (setq eldoc-idle-delay slime-autodoc-delay)
+  (setq eldoc-minor-mode-string " Autodoc")
+  (setq slime-autodoc-mode (eldoc-mode arg))
+  (when interactive
+    (message (format "Slime autodoc mode %s."
+                     (if slime-autodoc-mode "enabled" "disabled")))))
+
 (defun slime-autodoc-manually ()
   "Like slime-autodoc, but when called twice,
 or after slime-autodoc was already automatically called,
@@ -190,20 +206,14 @@ display multiline arglist"
                                     slime-autodoc-mode)
                                 t)))
 
-(make-variable-buffer-local (defvar slime-autodoc-mode nil))
-
-(defun slime-autodoc-mode (&optional arg)
-  (interactive (list (or current-prefix-arg 'toggle)))
-  (make-local-variable 'eldoc-documentation-function)
-  (make-local-variable 'eldoc-idle-delay)
-  (make-local-variable 'eldoc-minor-mode-string)
-  (setq eldoc-documentation-function 'slime-autodoc)
-  (setq eldoc-idle-delay slime-autodoc-delay)
-  (setq eldoc-minor-mode-string " Autodoc")
-  (setq slime-autodoc-mode (eldoc-mode arg))
-  (when (interactive-p)
-    (message (format "Slime autodoc mode %s."
-                     (if slime-autodoc-mode "enabled" "disabled")))))
+(defun slime-autodoc-maybe-enable ()
+  (when slime-use-autodoc-mode
+    (slime-autodoc-mode 1)
+    (setq slime-echo-arglist-function
+          (lambda ()
+            (if slime-autodoc-mode
+                (eldoc-message (slime-autodoc))
+              (slime-show-arglist))))))
 
 (defadvice eldoc-display-message-no-interference-p
     (after slime-autodoc-message-ok-p)

contrib/slime-banner.el
@@ -1,5 +1,5 @@
-(eval-and-compile
-  (require 'slime))
+(require 'slime)
+(require 'slime-repl)
 
 (define-slime-contrib slime-banner
   "Persistent header line and startup animation."

contrib/slime-cl-indent.el
@@ -33,6 +33,10 @@
 
 ;;; Code:
 
+(require 'slime) ; only for its cl-lib loading smartness
+(require 'cl-lib)
+(eval-when-compile (require 'cl))
+
 (defgroup lisp-indent nil
   "Indentation in Lisp."
   :group 'lisp)
@@ -252,16 +256,6 @@ style is used instead. Use `define-common-lisp-style' to define new styles.")
 ;;; Mark as safe when the style doesn't evaluate arbitrary code.
 (put 'common-lisp-style 'safe-local-variable 'common-lisp-safe-style-p)
 
-;;; If style is being used, that's a sufficient invitation to snag
-;;; the indentation function.
-(defun common-lisp-lisp-mode-hook ()
-  (let ((style (or common-lisp-style common-lisp-style-default)))
-    (when style
-      (set (make-local-variable 'lisp-indent-function)
-           'common-lisp-indent-function)
-      (common-lisp-set-style style))))
-(add-hook 'lisp-mode-hook 'common-lisp-lisp-mode-hook)
-
 ;;; Common Lisp indentation style specifications.
 (defvar common-lisp-styles (make-hash-table :test 'equal))
 
@@ -289,7 +283,7 @@ Ie. styles that will not evaluate arbitrary code on activation."
                               documentation)
   ;; Invalidate indentation methods cached in common-lisp-active-style.
   (maphash (lambda (k v)
-             (puthash k (copy-list v) common-lisp-styles))
+             (puthash k (cl-copy-list v) common-lisp-styles))
            common-lisp-styles)
   ;; Add/Redefine the specified style.
   (puthash stylename
@@ -342,8 +336,7 @@ Ie. styles that will not evaluate arbitrary code on activation."
 ;;; METHODS is the table of indentation methods --  including inherited
 ;;; ones -- for it. `common-lisp-active-style-methods' is reponsible
 ;;; for keeping this up to date.
-(make-variable-buffer-local 'common-lisp-active-style)
-(set-default 'common-lisp-active-style nil)
+(make-variable-buffer-local (defvar common-lisp-active-style nil))
 
 ;;; Makes sure common-lisp-active-style corresponds to common-lisp-style, and
 ;;; pick up redefinitions, etc. Returns the method table for the currently
@@ -516,6 +509,17 @@ none has been specified."
                            (common-lisp-style-names))
                  (string :tag "Other"))
   :group 'lisp-indent)
+
+;;; If style is being used, that's a sufficient invitation to snag
+;;; the indentation function.
+(defun common-lisp-lisp-mode-hook ()
+  (let ((style (or common-lisp-style common-lisp-style-default)))
+    (when style
+      (set (make-local-variable 'lisp-indent-function)
+           'common-lisp-indent-function)
+      (common-lisp-set-style style))))
+(add-hook 'lisp-mode-hook 'common-lisp-lisp-mode-hook)
+
 
 ;;;; The indentation specs are stored at three levels. In order of priority:
 ;;;;
@@ -555,8 +559,8 @@ none has been specified."
                                start (1- (point)))))))))
     pkg))
 
-(defun common-lisp-current-package-function 'common-lisp-guess-current-package
-  "Function used to the derive the package name to use for indentation at a
+(defvar common-lisp-current-package-function 'common-lisp-guess-current-package
+  "Used to derive the package name to use for indentation at a
 given point. Defaults to `common-lisp-guess-current-package'.")
 
 (defun common-lisp-symbol-package (string)
@@ -643,47 +647,6 @@ opening parenthesis of the loop."
       (when (looking-at "\\s-*;")
         (search-forward ";")
         (1- (current-column))))))
-
-(defun common-lisp-loop-part-indentation (indent-point state type)
-  "Compute the indentation of loop form constituents."
-  (let* ((loop-start (elt state 1))
-         (loop-indentation (save-excursion
-                             (goto-char loop-start)
-                             (if (eq type 'extended/split)
-                                 (- (current-column) 4)
-                               (current-column))))
-         (indent nil)
-         (re "\\(\\(#?:\\)?\\sw+\\|)\\|\n\\)"))
-    (goto-char indent-point)
-    (back-to-indentation)
-    (cond ((eq type 'simple/split)
-           (+ loop-indentation lisp-simple-loop-indentation))
-          ((eq type 'simple)
-           (+ loop-indentation 6))
-          ;; We are already in a body, with forms in it.
-          ((and (not (looking-at re))
-                (save-excursion
-                  (while (and (ignore-errors (backward-sexp) t)
-                              (not (looking-at re)))
-                    (setq indent (current-column)))
-                  (when (and indent
-                             (looking-at
-                              common-lisp-body-introducing-loop-macro-keyword))
-                    t)))
-           (list indent loop-start))
-          ;; Keyword-style or comment outside body
-          ((or lisp-loop-indent-forms-like-keywords
-               (looking-at re)
-               (looking-at ";"))
-           (if (and (looking-at ";")
-                    (let ((p (common-lisp-trailing-comment)))
-                      (when p
-                        (setq loop-indentation p))))
-               (list loop-indentation loop-start)
-             (list (+ loop-indentation 6) loop-start)))
-          ;; Form-style
-          (t
-           (list (+ loop-indentation 9) loop-start)))))
 
 ;;;###autoload
 (defun common-lisp-indent-function (indent-point state)
@@ -1469,6 +1432,47 @@ Cause subsequent clauses to be indented.")
 (defun common-lisp-indent-parse-state-prev (parse-state)
   (car (cdr (cdr parse-state))))
 
+(defun common-lisp-loop-part-indentation (indent-point state type)
+  "Compute the indentation of loop form constituents."
+  (let* ((loop-start (elt state 1))
+         (loop-indentation (save-excursion
+                             (goto-char loop-start)
+                             (if (eq type 'extended/split)
+                                 (- (current-column) 4)
+                               (current-column))))
+         (indent nil)
+         (re "\\(\\(#?:\\)?\\sw+\\|)\\|\n\\)"))
+    (goto-char indent-point)
+    (back-to-indentation)
+    (cond ((eq type 'simple/split)
+           (+ loop-indentation lisp-simple-loop-indentation))
+          ((eq type 'simple)
+           (+ loop-indentation 6))
+          ;; We are already in a body, with forms in it.
+          ((and (not (looking-at re))
+                (save-excursion
+                  (while (and (ignore-errors (backward-sexp) t)
+                              (not (looking-at re)))
+                    (setq indent (current-column)))
+                  (when (and indent
+                             (looking-at
+                              common-lisp-body-introducing-loop-macro-keyword))
+                    t)))
+           (list indent loop-start))
+          ;; Keyword-style or comment outside body
+          ((or lisp-loop-indent-forms-like-keywords
+               (looking-at re)
+               (looking-at ";"))
+           (if (and (looking-at ";")
+                    (let ((p (common-lisp-trailing-comment)))
+                      (when p
+                        (setq loop-indentation p))))
+               (list loop-indentation loop-start)
+             (list (+ loop-indentation 6) loop-start)))
+          ;; Form-style
+          (t
+           (list (+ loop-indentation 9) loop-start)))))
+
 (defun common-lisp-indent-loop-macro-1 (parse-state indent-point)
   (catch 'return-indentation
     (save-excursion
@@ -1831,7 +1835,7 @@ Cause subsequent clauses to be indented.")
      (complex-indent.2 (4 (&whole 4 &rest 1) &body))
      (complex-indent.3 (4 &body))))
   (with-temp-buffer
-    (insert-file "slime-cl-indent-test.txt")
+    (insert-file-contents "slime-cl-indent-test.txt")
     (goto-char 0)
     (let ((test-mark ";;; Test: ")
           (n 0)
@@ -1871,10 +1875,7 @@ Cause subsequent clauses to be indented.")
       (common-lisp-delete-style "common-lisp-indent-test")
       (message "%s tests OK." n))))
 
-;;; Run all tests:
-;;;   (common-lisp-run-indentation-tests t)
-;;;
-;;; Run specific test:
-;;;   (common-lisp-run-indentation-tests 77)
+(provide 'slime-cl-indent)
+(provide 'cl-indent)
 
 ;;; cl-indent.el ends here

commit 74fb6b9f1bb7b3a0bf3bafc3bb1299e31d72f2b2

contrib/slime-c-p-c.el: completion bugfix
+ Fixes "mapcar: Symbol's function definition is void:
  append-char-syntax" error when trying to complete #\

  This bug was introduced by Joao Tavora in:
  3467ebc9ead3408c21cd1d2ae119ac05e2a7e2d9

contrib/slime-c-p-c.el
@@ -274,7 +274,7 @@ current buffer."
     (let ((result (slime-eval `(swank:completions-for-character
                                 ,(cl-subseq prefix 2)))))
       (when (car result)
-        (list (mapcar 'append-char-syntax (car result))
+        (list (mapcar #'append-char-syntax (car result))
               (append-char-syntax (cadr result)))))))
 
 