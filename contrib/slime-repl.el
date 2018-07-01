dfa53c76f922c833fe96b38c52ada397bda0f32d
Stas Boukarev <stassats@gmail.com>
2018-07-01 03:21:40 +0300

    Bind C-x C-e to slime-eval-last-expression in slime-repl-mode-map.
    Something in the new Emacs can cause lisp-mode's binding to override
    the binding from slime-editing-mode.

--- contrib/slime-repl.el
+++ contrib/slime-repl.el
@@ -452,7 +452,8 @@ joined together."))
   ("\C-c\C-n" 'slime-repl-next-prompt)
   ("\C-c\C-p" 'slime-repl-previous-prompt)
   ("\C-c\C-z" 'slime-nop)
-  ("\C-cI" 'slime-repl-inspect))
+  ("\C-cI" 'slime-repl-inspect)
+  ("\C-x\C-e" 'slime-eval-last-expression))
 
 (slime-define-keys slime-inspector-mode-map
   ((kbd "M-RET") 'slime-inspector-copy-down-to-repl))
