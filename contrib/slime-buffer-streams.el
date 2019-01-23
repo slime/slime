478b183190eb35c639770f87bce4e974c243919b
Ed Langley <el-github@elangley.org>
2019-01-23 00:15:50 -0800

    Cleanup markers when corresponding buffer killed

--- contrib/slime-buffer-streams.el
+++ contrib/slime-buffer-streams.el
@@ -36,10 +36,22 @@
 (defun slime-buffer-streams--get-target-name (target)
   (format "*slime-target %s*" target))
 
+(make-variable-buffer-local
+ (defvar slime-buffer-stream-target nil))
+
+;; TODO: tell backend that the buffer has been closed, so it can close
+;;       the stream
+(defun slime-buffer-streams--cleanup-markers ()
+  (when slime-buffer-stream-target
+    (message "Removing target: %s" slime-buffer-stream-target)
+    (remhash slime-buffer-stream-target slime-output-target-to-marker)))
+
 (defun slime-buffer-streams--get-target-marker (target)
   (or (gethash target slime-output-target-to-marker)
       (with-current-buffer
           (generate-new-buffer (slime-buffer-streams--get-target-name target))
+        (setq slime-buffer-stream-target target)
+        (add-hook 'kill-buffer-hook 'slime-buffer-streams--cleanup-markers)
         (setf (gethash target slime-output-target-to-marker)
               (point-marker)))))
 
