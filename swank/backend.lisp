1c4681da458470987393ed5a2a4ab555a7d4d30c
Mark <evenson.not.org@gmail.com>
2019-03-01 14:46:57 +0100

    swank/backend: consolidate useful utilities for use by contribs
    (Alan Ruttenberg)
    
    Move WITH-STRUCT*, LCONS, LCONS*, LCONS-CDR, LLIST-RANGE, LLIST-SKIP,
    LLIST-TAKE and ILINE into swank/backend.lisp so that they may be used
    by code in <file:contrib/>.
    
    From
    <https://github.com/slime/slime/pull/477/commits/fde90d8230ed273dd97a08b3daa8f26440208b4f>.

--- swank/backend.lisp
+++ swank/backend.lisp
@@ -1190,6 +1190,63 @@ SPEC can be:
 
 ;;;; Inspector
 
+(defmacro with-struct* ((conc-name get obj) &body body)
+  (let ((var (gensym)))
+    `(let ((,var ,obj))
+       (macrolet ((,get (slot)
+                    (let ((getter (intern (concatenate 'string
+                                                       ',(string conc-name)
+                                                       (string slot))
+                                          (symbol-package ',conc-name))))
+                      `(,getter ,',var))))
+         ,@body))))
+
+;;;;; Lazy lists (moved from swank.lisp)
+
+(defstruct (lcons (:constructor %lcons (car %cdr))
+                  (:predicate lcons?))
+  car
+  (%cdr nil :type (or null lcons function))
+  (forced? nil))
+
+(defmacro lcons (car cdr)
+  `(%lcons ,car (lambda () ,cdr)))
+
+(defmacro lcons* (car cdr &rest more)
+  (cond ((null more) `(lcons ,car ,cdr))
+        (t `(lcons ,car (lcons* ,cdr ,@more)))))
+
+(defun lcons-cdr (lcons)
+  (with-struct* (lcons- @ lcons)
+    (cond ((@ forced?)
+           (@ %cdr))
+          (t
+           (let ((value (funcall (@ %cdr))))
+             (setf (@ forced?) t
+                   (@ %cdr) value))))))
+
+(defun llist-range (llist start end)
+  (llist-take (llist-skip llist start) (- end start)))
+
+(defun llist-skip (lcons index)
+  (do ((i 0 (1+ i))
+       (l lcons (lcons-cdr l)))
+      ((or (= i index) (null l))
+       l)))
+
+(defun llist-take (lcons count)
+  (let ((result '()))
+    (do ((i 0 (1+ i))
+         (l lcons (lcons-cdr l)))
+        ((or (= i count)
+             (null l)))
+      (push (lcons-car l) result))
+    (nreverse result)))
+
+(defun iline (label value)
+  `(:line ,label ,value))
+
+
 (defgeneric emacs-inspect (object)
   (:documentation
    "Explain to Emacs how to inspect OBJECT.
