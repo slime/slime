11ab65e48455056d17a0c0c78597dfb7b037f526
ego <jdoe.ego@gmail.com>
2020-07-05 15:16:11 +0200

    Implement import-symbol-at-point

--- contrib/swank-package-fu.lisp
+++ contrib/swank-package-fu.lisp
@@ -3,20 +3,26 @@
 
 (defslimefun package= (string1 string2)
   (let* ((pkg1 (guess-package string1))
-	 (pkg2 (guess-package string2)))
+         (pkg2 (guess-package string2)))
     (and pkg1 pkg2 (eq pkg1 pkg2))))
 
 (defslimefun export-symbol-for-emacs (symbol-str package-str)
   (let ((package (guess-package package-str)))
     (when package
       (let ((*buffer-package* package))
-	(export `(,(from-string symbol-str)) package)))))
+        (export `(,(from-string symbol-str)) package)))))
 
 (defslimefun unexport-symbol-for-emacs (symbol-str package-str)
   (let ((package (guess-package package-str)))
     (when package
       (let ((*buffer-package* package))
-	(unexport `(,(from-string symbol-str)) package)))))
+        (unexport `(,(from-string symbol-str)) package)))))
+
+(defslimefun import-symbol-for-emacs (symbol-str package-str)
+  (let ((package (guess-package package-str)))
+    (when package
+      (let ((*buffer-package* package))
+        (import (list (from-string symbol-str)) package)))))
 
 #+sbcl
 (defun list-structure-symbols (name)
