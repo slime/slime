c653d5db6dbbab839f90f4330d61f1754b82d1c1
Philipp Stephani <phst@google.com>
2022-05-09 15:26:57 +0200

    Use normal let* instead of deprecated lexical-let*

--- contrib/slime-asdf.el
+++ contrib/slime-asdf.el
@@ -162,7 +162,7 @@ buffer's working directory"
       (let* ((files (mapcar 'slime-from-lisp-filename
                             (slime-eval `(swank:asdf-system-files ,sys-name))))
              (multi-isearch-next-buffer-function
-              (lexical-let* 
+              (let* 
                   ((buffers-forward  (mapcar #'find-file-noselect files))
                    (buffers-backward (reverse buffers-forward)))
                 #'(lambda (current-buffer wrap)
