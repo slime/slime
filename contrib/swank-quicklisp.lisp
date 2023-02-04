2d52732fc093c3042ec3809b7b687dd200a90577
Grant Shangreaux <shoshin@cicadas.surf>
2023-02-04 15:41:23 -0600

    Add: local systems to swank-quicklisp systems list
    I am often using quicklisp to load local systems while
    developing. This seems like a useful addition to the functionality
    that ends up being used by the slime-quicklisp package.

--- contrib/swank-quicklisp.lisp
+++ contrib/swank-quicklisp.lisp
@@ -10,8 +10,10 @@
   "Returns the Quicklisp systems list."
   (if (member :quicklisp *features*)
       (let ((ql-dist-name (find-symbol "NAME" "QL-DIST"))
-            (ql-system-list (find-symbol "SYSTEM-LIST" "QL")))
-        (mapcar ql-dist-name (funcall ql-system-list)))
+            (ql-system-list (find-symbol "SYSTEM-LIST" "QL"))
+	    (ql-local-systems (find-symbol "LIST-LOCAL-SYSTEMS" "QL")))
+        (append (mapcar ql-dist-name (funcall ql-system-list))
+		(funcall ql-local-systems)))
       (error "Could not find Quicklisp already loaded.")))
 
 (provide :swank-quicklisp)
