e193bc5f3431a2f71f1d7a0e3f28e6dc4dd5de2d
Stas Boukarev <stassats@gmail.com>
2023-02-22 18:26:01 +0300

    I'm not your brother, pal.
    Closes #323

--- slime.el
+++ slime.el
@@ -1363,7 +1363,6 @@ The default condition handler for timer functions (see
     "Hack and be merry!"
     "Your hacking starts... NOW!"
     "May the source be with you!"
-    "Take this REPL, brother, and may it serve you well."
     "Lemonodor-fame is but a hack away!"
     "Are we consing yet?"
     ,(format "%s, this could be the start of a beautiful program."
