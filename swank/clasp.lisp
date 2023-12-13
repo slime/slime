b9442f133d6863defdcc881b9ed637356b3571d8
Tarn W. Burton <twburton@gmail.com>
2023-12-13 05:50:36 -0500

    clasp: Close temp file before compile-file

--- swank/clasp.lisp
+++ swank/clasp.lisp
@@ -304,10 +304,10 @@
             (warnings-p)
             (failure-p))
         (unwind-protect
-             (with-open-file (tmp-stream tmp-file :direction :output
-                                                  :if-exists :supersede)
-               (write-string string tmp-stream)
-               (finish-output tmp-stream)
+             (progn
+               (with-open-file (tmp-stream tmp-file :direction :output
+                                                    :if-exists :overwrite)
+                 (write-string string tmp-stream))
                (multiple-value-setq (fasl-file warnings-p failure-p)
                  (let ((truename (or filename (note-buffer-tmpfile tmp-file buffer))))
                    (compile-file tmp-file
