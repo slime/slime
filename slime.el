d85dd9899f7524513c8ad1bd47b0dd248b1915cb
Stas Boukarev <stassats@gmail.com>
2024-01-24 13:46:26 +0300

    2.29.1

--- slime.el
+++ slime.el
@@ -3,7 +3,7 @@
 ;; URL: https://github.com/slime/slime
 ;; Package-Requires: ((cl-lib "0.5") (macrostep "0.9"))
 ;; Keywords: languages, lisp, slime
-;; Version: 2.29
+;; Version: 2.29.1
 
 ;;;; License and Commentary
 
